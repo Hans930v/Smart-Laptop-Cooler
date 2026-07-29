d1b8375e45411fad1afde56cc695a768a951a824
Hansoy <hansvillafranca9@gmail.com>
2026-07-29 23:47:07 +0800

    Update SmartLaptopCooler.ino

--- Firmware/SmartLaptopCooler/SmartLaptopCooler.ino
+++ Firmware/SmartLaptopCooler/SmartLaptopCooler.ino
@@ -6,7 +6,7 @@
 
 BluetoothSerial SerialBT;
 
-// SH1106 128x32, I2C, hardware I2C, no reset pin
+// SH1106 128x64, I2C, hardware I2C, no reset pin
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 // ===== Pin config =====
